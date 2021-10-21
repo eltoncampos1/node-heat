993938675fa0841ed4b5efb0e24c4b01bba4c60f
eltoncampos1 <eltoncampos36@gmail.com>
2021-10-21 18:55:05 -0300

    feat: create last 3 messages

---
+++ src/services/GetLastThreeMessages.ts
@@ -0,0 +1,18 @@
+import prismaClient from "../prisma";
+
+class GetLastThreeMessages {
+  async execute() {
+    const messages = await prismaClient.message.findMany({
+      take: 3,
+      orderBy: {
+        created_at: "desc",
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    return messages;
+  }
+}
+export { GetLastThreeMessages };
