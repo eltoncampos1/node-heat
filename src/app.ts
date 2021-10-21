cdd0da2dfc07b618aa8622b7d50b1946ebf40c87
eltoncampos1 <eltoncampos36@gmail.com>
2021-10-21 16:03:40 -0300

    feat: create user authentication

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
-import express, { request } from "express";
+import express from "express";
+import { router } from "./routes/routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 app.get("/github", (request, response) => {
   response.redirect(
