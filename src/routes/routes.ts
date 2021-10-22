61cf3d6f3a09603f94d0d0dc13585407de62721d
eltoncampos1 <eltoncampos36@gmail.com>
2021-10-22 09:31:10 -0300

    feat: profile user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "../controllers/AuthenticateUserController";
 import { CreateMessageController } from "../controllers/CreateMessageController";
 import { GetLastThreeMessagesController } from "../controllers/GetLastThreeMessagesController";
+import { ProfileUserController } from "../controllers/ProfileUserController";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
 const router = Router();
@@ -14,5 +15,6 @@ router.post(
 );
 
 router.get("/messages/last3", new GetLastThreeMessagesController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
